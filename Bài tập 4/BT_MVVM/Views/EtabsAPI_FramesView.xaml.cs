5b7db997a11011e529dae7d653349312cb5d172b
dainaham-dhn <namsaygaming@gmail.com>
2026-03-30 05:38:32 +0700

    MVVM ( vẫn lỗi chưa xong)

---
+++ Bài tập 4/BT_MVVM/Views/EtabsAPI_FramesView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using BT_MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BT_MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for EtabsAPI_FramesView.xaml
+    /// </summary>
+    public partial class EtabsAPI_FramesView : Window
+    {
+        public EtabsAPI_FramesView()
+        {
+            InitializeComponent();
+
+        }
+
+
+    }
+}
